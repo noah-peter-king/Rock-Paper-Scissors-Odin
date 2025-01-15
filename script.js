22673cea8e2600da3481e4cb4c4d3892e23c8e59
noah-peter-king <177217902+noah-peter-king@users.noreply.github.com>
2025-01-15 13:56:27 +1100

    Button functionality and result div added Need to make it possible for player to play

--- script.js
+++ script.js
@@ -1,88 +1,92 @@
 let humanScore = 0
 let computerScore = 0
+let numOfRounds = 0
+let buttons = document.querySelectorAll('button');
 
 function getComputerChoice(){
     computerChoice = Math.floor(Math.random() * 3)
-
     if (computerChoice === 1){
-        console.log("rock")
-        return "rock"
+        console.log('computer choice is rock')
+        return 'rock'
     } else if (computerChoice === 2){
-        console.log("paper")
-        return "paper"
+        console.log('computer choice is paper')
+        return 'paper'
     } else {
-        console.log("scissors")
-        return "scissors"
+        console.log('computer choice is scissors')
+        return 'scissors'
     } 
 };
 
-function getHumanChoice(){
-    humanChoice = prompt("Rock, Paper or Scissors").toLowerCase()
-    if (humanChoice === "rock"){
-        humanChoice = "rock"
-        console.log("rock")
-        return "rock"
-    } else if (humanChoice === "paper"){
-        humanChoice = "paper"
-        console.log("paper")
-        return "paper"
-    } else if (humanChoice === "scissors") {
-        humanChoice = "scissors"
-        console.log("scissors")
-        return "scissors"
-    }
-};
+humanChoice = buttons.forEach(function(elem) {
+    elem.addEventListener("input", (event) =>{
+        let target = event.target;
+        switch(target.id){
+            case 'rock' :
+                humanChoice = 'rock';
+                break;
+            case 'paper' :
+                humanChoice = 'paper';
+                break;
+            case 'scissors' :
+                humanChoice = 'scissors';
+                break;
+        };console.log('Human Choice Is '+ humanChoice)
+    });
+});
 
-/*function playRound(humanChoice, computerChoice) {
-    getHumanChoice();
-    getComputerChoice();
-    if (humanChoice === computerChoice){
-        return " Round Draw!"
-    } else if (humanChoice === "paper" && computerChoice === "rock"
-        || humanChoice === "scissors" && computerChoice === "paper"
-        || humanChoice === "rock" && computerChoice === "scissors") {
-        humanScore++
-        console.log("Player Wins Round!"+ humanScore)
-    } else {
-        computerScore++
-        console.log("Computer Wins Round!"+ computerScore)
-    }
-  }; */
 
-// the function playRound is meant to run 5 times and annouce the winner in the end
-// NOT waiting until one score equals 5
-function playGame() {
-    function playRound(humanChoice, computerChoice) {
+    /*if (humanChoice === 'rock'){
+        humanChoice = 'rock'
+        console.log('rock')
+        return 'rock'
+    } else if (humanChoice === 'paper'){
+        humanChoice = 'paper'
+        console.log('paper')
+        return 'paper'
+    } else if (humanChoice === 'scissors') {
+        humanChoice = 'scissors'
+        console.log('scissors')
+        return 'scissors'
+    }*/
+
+function playRound(humanChoice, computerChoice) {
+    console.log('playing round')
+        numOfRounds++
+        roundNum.textContent = numOfRounds
+        getComputerChoice();
         if (humanChoice === computerChoice){
-            console.log("Round Draw!")
-        } else if (humanChoice === "paper" && computerChoice === "rock"
-            || humanChoice === "scissors" && computerChoice === "paper"
-            || humanChoice === "rock" && computerChoice === "scissors") {
+            result.textContent = ("Round Draw! Human Score Is "+humanScore
+                + "Computer Score Is " +computerScore);
+        }
+        else if (humanChoice === 'paper' && computerChoice === 'rock'
+            || humanChoice === 'scissors' && computerChoice === 'paper'
+            || humanChoice === 'rock' && computerChoice === 'scissors') {
             humanScore++
-            console.log("Player Wins Round! "+ humanScore)
-        } else {
+            result.textContent = ("Player Wins Round! Human Score Is "+humanScore
+                + "Computer Score Is " +computerScore
+            )
+        } else 
+        {   
             computerScore++
-            console.log("Computer Wins Round! "+ computerScore)
+            result.textContent = ("Computer Wins Round! Human Score Is "+humanScore
+                + " Computer Score Is " +computerScore) 
         }
-      }; 
+
     
-    for (let i = 0; i < 5; i++) {
-        const humanSelection = getHumanChoice();
-        const computerSelection = getComputerChoice();
-        playRound(humanSelection, computerSelection);
-        console.log("Human score is "+ humanScore + ", Computer score is "+ computerScore)
-    } 
-    while (true){
+
+    if(numOfRounds % 5 === 0)
         if (computerScore === humanScore){
-            console.log("Noone Wins The Game! ")
-            break;
+            result.textContent = ("Noone Wins The Game! Human score is "
+                + humanScore + ", Computer score is "+ computerScore )
         } else if (humanScore < computerScore){
-            console.log("Computer Wins The Game!")
-            break;
+            result.textContent = ("Computer Wins The Game! Human score is "
+                + humanScore + ", Computer score is "+ computerScore)
         } else {
-            console.log("Human Wins The Game")
-            break;
+            result.textContent = ("Human Wins The Game Human score is "
+                + humanScore + ", Computer score is "+ computerScore)
         }
-    }
 }
-playGame();
+
+  buttons.forEach(function(elem) {
+    elem.addEventListener("click", () => playRound(humanChoice, getComputerChoice));
+});
